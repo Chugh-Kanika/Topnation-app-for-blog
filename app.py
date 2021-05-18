976811f6c991ae84632d43e673cfcf184d7ce6bb
Chugh-Kanika <73135217+Chugh-Kanika@users.noreply.github.com>
2021-05-18 16:12:36 -0700

    updated to solve error in deployment

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from pathlib import Path
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 # get relative data folder
 
 
